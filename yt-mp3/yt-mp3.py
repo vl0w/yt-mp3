35fe4f5af4d2ba0323ca8317face6f4714660912
Jonas Hansen <jonas.hansen@bluewin.ch>
2015-12-16 15:42:54 +0100

    Ignore .log files when printing warning

--- yt-mp3/yt-mp3.py
+++ yt-mp3/yt-mp3.py
@@ -93,7 +93,7 @@ def main(argv):
 
     def tag_file(file_path: str):
         if not file_path.endswith(".mp3"):
-            if not file_path.endswith(".txt") or file_path.endswith(".log"):
+            if not file_path.endswith(".txt") and not file_path.endswith(".log"):
                 log.warn("Detected invalid file {0}. Only mp3 allowed".format(file_path))
         else:
             try:
