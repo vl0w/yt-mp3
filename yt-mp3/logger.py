ae21d93778dd72acb120f87ff069f0224f507fc6
Jonas Hansen <jonas.hansen@bluewin.ch>
2016-02-02 15:02:13 +0100

    Nothing to remove

--- yt-mp3/logger.py
+++ yt-mp3/logger.py
@@ -1,12 +1,9 @@
-import os, datetime
+import datetime
 
 
 class Logger:
-    def __init__(self, log_file_path: str, append_to_existing_logs=True):
+    def __init__(self, log_file_path: str):
         self.log_file_path = log_file_path
-
-        if not append_to_existing_logs:
-            self.clear_all_logs()
 
     def debug(self, message: str):
         self.__log_to_console_and_file(message, "DEBUG")
@@ -23,12 +20,9 @@ class Logger:
     def __log_to_console_and_file(self, message: str, level: str):
         full_message = "{0}: {1} | {2}".format(level, datetime.datetime.now(), message)
         print(full_message.encode("utf-8"))
-        self.log_to_file(full_message)
+        self.__log_to_file(full_message)
 
-    def clear_all_logs(self):
-        os.remove(self.log_file_path) if os.path.exists(self.log_file_path) else None
-
-    def log_to_file(self, message):
+    def __log_to_file(self, message):
         with open(self.log_file_path, "ab+") as file:
             file.write(message.encode("utf8", "surrogateescape"))
             file.write("\n".encode("utf8"))
